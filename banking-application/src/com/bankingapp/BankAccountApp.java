7b43104272a5c7f874bdbd10382c7fa6d3316cd2
collin <collin@localhost>
2021-05-09 15:51:16 -0400

    Added a CSV reader

--- banking-application/src/com/bankingapp/BankAccountApp.java
+++ banking-application/src/com/bankingapp/BankAccountApp.java
@@ -1,22 +1,44 @@
 package com.bankingapp;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.bankingapp.accounts.Account;
 import com.bankingapp.accounts.Checking;
 import com.bankingapp.accounts.Savings;
+import com.bankingapp.utilities.CSV;
 
 public class BankAccountApp {
 
 	public static void main(String[] args) {
 		
-		var ch1 = new Checking("Collin Pointer", "321759087", 1000.00);
-		ch1.displayAccountInfo();
+		List<Account> accounts = new LinkedList<Account>();
 		
-		System.out.println("\n************************************\n");
+		String file = "/Users/collin/Desktop/NewBankAccounts.csv";
+		List<String[]> newAccounts = CSV.read(file);
 		
-		var sv1 = new Savings("Kyle Pointer", "890673421", 2500.00);
-		sv1.displayAccountInfo();
-		sv1.deposit(10);
-		ch1.transer(sv1, 500);
-		sv1.compound();
+		for (String[] account : newAccounts) {
+			String name = account[0];
+			String ssn = account[1];
+			String accountType = account[2];
+			double initialDeposit = Double.parseDouble(account[3]);
+			
+			if (accountType.equals("Checking")) {
+				accounts.add(new Checking(name, ssn, initialDeposit));
+			}
+			else if (accountType.equals("Savings")) {
+				accounts.add(new Savings(name, ssn, initialDeposit));
+			}
+			else {
+				System.out.println("ERROR READING ACCOUNT TYPE");
+			}
+		}
+		
+		for (Account acc : accounts) {
+			acc.displayAccountInfo();
+			System.out.println("\n***********************************");
+		}
+		
 	}
 		
 }
---
+++ banking-application/src/com/bankingapp/utilities/CSV.java
@@ -0,0 +1,37 @@
+package com.bankingapp.utilities;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CSV {
+
+	// Reads from a CSV file and returns input as a list
+	public static List<String[]> read(String file) {
+		List<String[]> data = new LinkedList<String[]>();
+		String dataRow;
+		
+		try {
+			@SuppressWarnings("resource")
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			
+			while ((dataRow = reader.readLine()) != null) {
+				String[] dataRecords = dataRow.split(",");
+				data.add(dataRecords);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("COULD NOT LOCATE FILE");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("COULD NOT READ FILE");
+			e.printStackTrace();
+		}
+		
+		return data;
+	}
+	
+}
